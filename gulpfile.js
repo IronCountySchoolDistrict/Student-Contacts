7f91c8f94bae6228de130db9e30143a85132570b
Nathan Jones <Purkinje90@gmail.com>
2015-06-11 09:39:53 -0600

    Changed gulp-ps-tasks import path

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
 var gulp = require("gulp");
-var plugins = require("gulp-load-plugins")();
-plugins.del = require("del");
 
-require("../gulp-ps-tasks/tasks")(gulp, plugins);
+require("../tasks")(gulp);
