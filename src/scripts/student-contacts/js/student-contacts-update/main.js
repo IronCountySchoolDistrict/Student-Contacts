c6294512a55b46f2b3ed5d687be08e516d2f8af9
Nathan Jones <Purkinje90@gmail.com>
2015-09-11 15:55:31 -0600

    Removed preprocessor calls to insert cdn url. Started porting AJAX calls to escl

--- src/scripts/student-contacts/js/student-contacts-update/main.js
+++ src/scripts/student-contacts/js/student-contacts-update/main.js
@@ -1,28 +1,30 @@
 /*global require,loadingDialogInstance*/
-require.config({
-    paths: {
-        // app modules
-        'tableModule': '/scripts/student-contacts/js/student-contacts-update/table-module',
-        'service': '/scripts/student-contacts/js/student-contacts-update/service',
-        'actions': '/scripts/student-contacts/js/student-contacts-update/actions',
-        'config': '/scripts/student-contacts/js/student-contacts-update/config',
-        'tooltip': '/scripts/student-contacts/js/student-contacts-update/tooltip',
-        'jquery.inputmask': '/scripts/student-contacts/lib/jquery.inputmask',
-        'inputmask': '/scripts/student-contacts/lib/inputmask',
-        'dependencyLib': '/scripts/student-contacts/lib/dependencyLib',
-        'inputmask.extensions': '/scripts/student-contacts/lib/inputmask.extensions',
+export var main = function() {
+  require.config({
+      paths: {
+          // app modules
+          'tableModule': '/scripts/student-contacts/js/student-contacts-update/table-module',
+          'service': '/scripts/student-contacts/js/student-contacts-update/service',
+          'actions': '/scripts/student-contacts/js/student-contacts-update/actions',
+          'config': '/scripts/student-contacts/js/student-contacts-update/config',
+          'tooltip': '/scripts/student-contacts/js/student-contacts-update/tooltip',
+          'jquery.inputmask': '/scripts/student-contacts/lib/jquery.inputmask',
+          'inputmask': '/scripts/student-contacts/lib/inputmask',
+          'dependencyLib': '/scripts/student-contacts/lib/dependencyLib',
+          'inputmask.extensions': '/scripts/student-contacts/lib/inputmask.extensions',
 
-        'parsley': '//cdnjs.cloudflare.com/ajax/libs/parsley.js/2.0.5/parsley',
-        'underscore': 'underscore/underscore'
-    }
-});
+          'parsley': '//cdnjs.cloudflare.com/ajax/libs/parsley.js/2.0.5/parsley',
+          'underscore': 'underscore/underscore'
+      }
+  });
 
-require(['tableModule', 'actions', 'tooltip'],
-    function (tableModule, actions, tooltip) {
-        'use strict';
-        loadingDialogInstance.open();
-        tableModule.main();
-        actions.main();
-        tooltip.main();
-        $j('#btn-student-contacts').attr({'class': 'selected'});
-    });
+  require(['tableModule', 'actions', 'tooltip'],
+      function (tableModule, actions, tooltip) {
+          'use strict';
+          loadingDialogInstance.open();
+          tableModule.main();
+          actions.main();
+          tooltip.main();
+          $j('#btn-student-contacts').attr({'class': 'selected'});
+      });
+};
