76c557ebca1f85c98befecc41fadd455475f32dd
basiczombie <issacgable@gmail.com>
2016-06-08 08:20:57 -0600

    refactor start

--- src/scripts/student-contacts/js/sc-link.js
+++ src/scripts/student-contacts/js/sc-link.js
@@ -1,6 +1,6 @@
 /*global $j*/
-(function() {
-	var linkTemplate = $j($j('#sc-link-template').html());
-	var linkSelect = $j('#btn-accountPreferences');
+(function($) {
+	var linkTemplate = $($('#sc-link-template').html());
+	var linkSelect = $('#btn-accountPreferences');
 	linkTemplate.insertAfter(linkSelect);
-}());
+}($j));
