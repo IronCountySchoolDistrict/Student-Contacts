5450f302b9e6a3dde80fa0a36832234fa954f97c
Nathan Jones <nathan.jones@ironmail.org>
2016-06-08 10:29:49 -0600

    Use es6 modules and compile to AMD-style imports

--- src/scripts/student-contacts/js/student-contacts/pending-contacts.js
+++ src/scripts/student-contacts/js/student-contacts/pending-contacts.js
@@ -1,19 +1,20 @@
-define(['underscore', 'jquery'], function(_, $) {
-  return function main() {
-    $.getJSON('/admin/students/contacts/pending-contacts/getAllStudentStagingContacts.json.html?studentsdcid=' + psData.studentdcid, function(stagingContacts) {
-      if (stagingContacts.length > 0) {
-        stagingContacts.pop();
-        $.each(stagingContacts, function(index, stagingContact) {
-          if (!$.isEmptyObject(stagingContact)) {
-            var contactTemplate = $('#staging-contact-template').html();
-            var renderedTemplate = _.template(contactTemplate, {
-              contact: stagingContact
-            });
-            var insertSelector = $('#staging-contacts-list');
-            insertSelector.append(renderedTemplate);
-          }
-        });
-      }
-    });
-  };
-});
+import _ from 'underscore';
+import $ from 'jquery';
+
+export function main() {
+  $.getJSON('/admin/students/contacts/pending-contacts/getAllStudentStagingContacts.json.html?studentsdcid=' + psData.studentdcid, function(stagingContacts) {
+    if (stagingContacts.length > 0) {
+      stagingContacts.pop();
+      $.each(stagingContacts, function(index, stagingContact) {
+        if (!$.isEmptyObject(stagingContact)) {
+          var contactTemplate = $('#staging-contact-template').html();
+          var renderedTemplate = _.template(contactTemplate, {
+            contact: stagingContact
+          });
+          var insertSelector = $('#staging-contacts-list');
+          insertSelector.append(renderedTemplate);
+        }
+      });
+    }
+  });
+};
